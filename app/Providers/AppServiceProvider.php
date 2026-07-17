add074d9821508e28c5f16b076ab1bfff685e976
Sérgio Silva <sergiolimasilva12@gmail.com>
2026-07-16 23:25:50 -0300

    vamos

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -20,7 +20,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        if ($this->app->environment('production')) {
+        // Força o carregamento de CSS/JS com HTTPS em produção
+        if (env('APP_ENV') === 'production') {
             URL::forceScheme('https');
         }
     }
